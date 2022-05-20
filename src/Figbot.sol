650f4143d0f4f63502d7fce36ed2503322ecfb6a
Pradhumna Pancholi <pnpancholi@protonmail.com>
2022-05-19 22:12:09 -0400

    initial contrat setup and mint function

---
+++ src/Figbot.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/utils/Counters.sol";
+contract Figbot is ERC721URIStorage, Ownable{
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    uint256 public constant MAX_SUPPLY = 100;
+    uint256 public constant COST = 0.69 ether;
+    string constant TOKEN_URI = "ipfs://QmWQLuGKWiho3cR8sEqZQGYfgEUAJrogyGkfC9ts2eQ4LF";
+    constructor() ERC721("Figbot", "FBT") {}
+
+    function mint() public payable {
+        require(msg.value >= COST, "Insufficient funds");
+        require(MAX_SUPPLY > _tokenIds.current(), "You can not mint anymore");
+
+        //increment tokenId - started at 0//
+        _tokenIds.increment();
+        _safeMint(msg.sender, _tokenIds.current());
+        _setTokenURI(_tokenIds.current(), TOKEN_URI);
+    }
+
+
+
+}
