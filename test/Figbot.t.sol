51e8cccce70fffb387f1e4bcc6caf8b5c616557d
Pradhumna Pancholi <pnpancholi@protonmail.com>
2022-05-20 22:55:47 -0400

    added tests

--- test/Figbot.t.sol
+++ test/Figbot.t.sol
@@ -2,12 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
 import "../src/Figbot.sol";
+
+
 contract ContractTest is Test {
 
     Figbot figbot;
+    address owner = address(0x1223);
     function setUp() public {
-       figbot = new Figbot(); 
+        vm.startPrank(owner);
+        figbot = new Figbot();
+        vm.stopPrank(); 
     }
 
     function testMaxSupply() public {
@@ -15,10 +21,24 @@ contract ContractTest is Test {
     }
 
     function testMint() public {
-        
+        // swtich address/account and give it some balance //
+        address alice = address(0x1889);
+        hoax(alice, 1 ether);
+        figbot.mint{value: 0.8 ether}();
+        assertEq(figbot.balanceOf(alice), 1);
     }
     
-    function testWithdraw() public {
-        
+    function testWithdrawFromOwner() public {
+        // switch to a diffent acount and mint a nft to have funds in contrat//
+        address bob = address(0x1778);
+        vm.startPrank(bob);
+        vm.deal(bob, 1 ether);
+        figbot.mint{value: 0.69 ether}();
+        assertEq(figbot.balanceOf(bob), 1);
+        vm.stopPrank();
+        //call withdraw with owner address//
+        vm.startPrank(owner);
+        figbot.withdrawFunds();
+        assertEq(owner.balance, 0.69  ether);
     }
 }
