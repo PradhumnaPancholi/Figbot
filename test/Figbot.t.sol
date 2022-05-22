dfaea846c259d50324e9c846c2fba6054ad91836
Pradhumna Pancholi <pnpancholi@protonmail.com>
2022-05-22 16:57:17 -0400

    organized test and added fail test

--- test/Figbot.t.sol
+++ test/Figbot.t.sol
@@ -10,6 +10,8 @@ contract ContractTest is Test {
 
     Figbot figbot;
     address owner = address(0x1223);
+    address alice = address(0x1889);
+    address bob = address(0x1778);
     function setUp() public {
         vm.startPrank(owner);
         figbot = new Figbot();
@@ -20,17 +22,28 @@ contract ContractTest is Test {
         assertEq(figbot.MAX_SUPPLY(), 100);
     }
 
+    // test for a succesfull mint // 
     function testMint() public {
         // swtich address/account and give it some balance //
-        address alice = address(0x1889);
-        hoax(alice, 1 ether);
+        vm.startPrank(alice);
+        vm.deal(alice, 1 ether);
         figbot.mint{value: 0.8 ether}();
+        vm.stopPrank();
         assertEq(figbot.balanceOf(alice), 1);
+    }
+
+
+    //test for unsuccesfull mint due to insuffucient funds//
+    function testFailMint() public {
+        vm.startPrank(bob);
+        vm.deal(bob, 0.5 ether);
+        figbot.mint{value: 0.69 ether};
+        vm.stopPrank();
+        assertEq(figbot.balanceOf(bob), 1);
     }
     
     function testWithdrawFromOwner() public {
         // switch to a diffent acount and mint a nft to have funds in contrat//
-        address bob = address(0x1778);
         vm.startPrank(bob);
         vm.deal(bob, 1 ether);
         figbot.mint{value: 0.69 ether}();
@@ -41,4 +54,5 @@ contract ContractTest is Test {
         figbot.withdrawFunds();
         assertEq(owner.balance, 0.69  ether);
     }
+
 }
